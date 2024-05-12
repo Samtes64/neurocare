742d69ea7a35fe3949f00046e922a5640c451056
Samuel Tesfaye <samtes64@gmail.com>
2024-05-12 14:25:56 +0300

    fixing teh assessment and the result page

--- client/src/components/for_assessment/ResultPage.jsx
+++ client/src/components/for_assessment/ResultPage.jsx
@@ -1,27 +1,45 @@
 import React from "react";
 import Button from "./Button";
 
-const ResultPage = ({ score, quizzes, onClickTry }) => (
-	<div className="h-screen flex flex-col justify-center items-center gap-3 pb-8">
-		<img
-			src="/yayy.gif"
-			className=" w-48  mt-auto md:mt-0 ms-11 "
-			alt=""
-		/>
+const ResultPage = ({ prediction, score, onClickTry }) => {
+  let message = "";
+  let scoreColor = "";
 
-		<span className=" text-3xl">Congrats</span>
+  if (prediction[0] === "None") {
+    message = "You most likely don't have anything.";
+  } else {
+    message = `There is a `;
+  }
 
-		<h2 className=" font-bold text-5xl">{score * (100 / quizzes.length)}% Score</h2>
+  if (score < 40) {
+    scoreColor = "text-green-500";
+  } else if (score >= 40 && score <= 70) {
+    scoreColor = "text-yellow-500";
+  } else {
+    scoreColor = "text-red-500";
+  }
 
-		<span className="block text-lg font-light">Quiz completed successfully.</span>
+  return (
+    <div className="h-screen flex flex-col justify-center items-center gap-3 pb-8">
+      <img src="/yayy.gif" className=" w-48  mt-auto md:mt-0 ms-11 " alt="" />
 
-		<Button
-			customStyle=" w-full md:w-fit bg-accent text-white dark:bg-primary px-8 py-2 rounded-full mt-auto md:mt-14 "
-			onClickButton={onClickTry}
-		>
-			Let's do it again
-		</Button>
-	</div>
-);
+      <span className=" text-4xl font-bold">Results</span>
+
+      <h2 className="text-3xl text-center">
+        {message}
+        {prediction[0] !== "None" && <span className={scoreColor}>{score}%</span>}
+    
+        {prediction[0] !== "None" && ` chance of you having ${prediction}`}
+      </h2>
+
+      <Button
+        customStyle=" w-full md:w-fit bg-accent text-white dark:bg-primary px-8 py-2 rounded-full mt-auto md:mt-14 "
+        onClickButton={onClickTry}
+      >
+        Let's do it again
+      </Button>
+    </div>
+  );
+};
 
 export default ResultPage;
--- client/src/pages/Assessment.jsx
+++ client/src/pages/Assessment.jsx
@@ -5,222 +5,218 @@ import AnswerList from "../components/for_assessment/AnswerList";
 import Question from "../components/for_assessment/Question";
 import ResultPage from "../components/for_assessment/ResultPage";
 import Loader from "../components/for_assessment/Loader";
-
-
+import axios from "axios";
 
 
 
 const exampleData = [
+    {
+      category: "Demographic",
+      choices: ["Male", "Female"],
+      question: "What is your gender?",
+      name: "gender",
+    },
+    {
+      category: "Demographic",
+      choices: ["13-19", "20-26", "27-33", "34-44", "45 or more"],
+      question: "Which age group do you belong too?",
+      name: "age",
+    },
+    {
+      category: "Demographic",
+      choices: ["University", "College", "Professional", "School", "Unemployed"],
+      question: "What is your level of education?",
+      name: "education",
+    },
+    {
+      category: "Demographic",
+      choices: ["single", "married", "divorced"],
+      question: "What is your marital status?",
+      name: "marital",
+    },
+    {
+      category: "Demographic",
+      choices: ["<10", "<20", "<30", "30+", "50+"],
+      question: "What is your approximate monthly income? (in term of thousands)",
+      name: "income",
+    },
+    {
+      category: "Loan",
+      choices: ["yes", "no"],
+      question: "Do you have any outstanding loans?",
+      name: "loan",
+    },
+    {
+      category: "Social and Emotional Well-being",
+      choices: ["none", "1", "2", "3", "3+"],
+      question: "How many close friends do you have?",
+      name: "friend_no",
+    },
+    {
+      category: "Social and Emotional Well-being",
+      choices: ["yes", "no", "maybe"],
+      question: "Does your friends help you when you need help?",
+      name: "friend_help",
+    },
+    {
+      category: "Social and Emotional Well-being",
+      choices: ["0", "1", "2", "3", "3+"],
+      question: "How many times a week do you interact with your friends?",
+      name: "friend_interact",
+    },
+    {
+      category: "Social and Emotional Well-being",
+      choices: ["yes", "no", "maybe", "idk"],
+      question: "Do you feel comfortable sharing your feelings with others?",
+      name: "share_feel",
+    },
+    {
+      category: "Social and Emotional Well-being",
+      choices: ["yes", "no", "maybe"],
+      question: "Do you have someone to talk to when you need support?",
+      name: "have_someone",
+    },
+    {
+      category: "Social and Emotional Well-being",
+      choices: ["never", "maybe", "no", "sometimes", "yes"],
+      question: "Do you often feel lonely?",
+      name: "lonely",
+    },
+    {
+      category: "Social and Emotional Well-being",
+      choices: ["yes", "no"],
+      question: "Have you ever been bullied?",
+      name: "bullied",
+    },
+    {
+      category: "Social and Emotional Well-being",
+      choices: ["yes", "no", "maybe"],
+      question: "Do you feel supported by your family?",
+      name: "family_support",
+    },
+    {
+      category: "Behavioral Patterns",
+      choices: ["yes", "no"],
+      question: "Do you compare your life to others?",
+      name: "compare_life",
+    },
+    {
+      category: "Behavioral Patterns",
+      choices: ["<1 hour", "<2 hours", "<3 hours", "3+ hours"],
+      question: "How much time do you spend on social media daily?",
+      name: "social_media",
+    },
+    {
+      category: "Sleep and Lifestyle",
+      choices: ["none", "1", "2", "3", "3+"],
+      question: "How many times a week do you hang out with friends?",
+      name: "hangout",
+    },
+    {
+      category: "Sleep and Lifestyle",
+      choices: ["<10", "<15", "<20", "<24", "24"],
+      question: "How many hours a day do you stay at home?",
+      name: "home_time",
+    },
+    {
+      category: "Sleep and Lifestyle",
+      choices: ["yes", "no"],
+      question: "Do you consider yourself religious/spiritual?",
+      name: "religious",
+    },
+    {
+      category: "Goals and Outlook",
+      choices: ["yes", "no", "maybe"],
+      question: "Do you have specific goals for yourself?",
+      name: "goal",
+    },
+    {
+      category: "Mental Health History",
+      choices: ["yes", "no"],
+      question: "Have you ever feel suicidal ?",
+      name: "suicidal",
+    },
+    {
+      category: "Sleep and Lifestyle",
+      choices: ["yes", "no"],
+      question: "Do you have a sleeping disorder?",
+      name: "sleep_disorder",
+    },
 
-
     {
-        "category": "Demographic",
-        "choices": ["Male", "Female"],
-        "question": "What is your gender?",
-        "name":"gender"
+      category: "Emotional Well-being",
+      choices: ["yes", "no", "maybe"],
+      question: "Are you currently in love with someone?",
+      name: "love_someone",
     },
     {
-        "category": "Demographic",
-        "choices": ["13-19", "20-26", "27-33", "34-44", "45 or more"],
-        "question": "Which age group do you belong too?",
-        "name":"age"
+      category: "Emotional Well-being",
+      choices: ["yes", "no", "maybe"],
+      question: "Have you recently lost someone close to you?",
+      name: "die_someone",
     },
     {
-        "category": "Demographic",
-        "choices": ["University", "College", "Professional", "School", "Unemployed"],
-        "question": "What is your level of education?",
-        "name":"education"
+      category: "Mental Health History",
+      choices: ["yes", "no", "maybe"],
+      question: "Do you have thoughts that tell you to do things?",
+      name: "thoughts_command",
     },
     {
-        "category": "Demographic",
-        "choices": ["single", "married", "divorced"],
-        "question": "What is your marital status?",
-        "name":"marital"
+      category: "Mental Health History",
+      choices: ["yes", "no", "maybe"],
+      question: "Have you ever engaged in self-harm?",
+      name: "self_harm",
     },
     {
-        "category": "Demographic",
-        "choices": ["<10", "<20", "<30", "30+" ,"50+"],
-        "question": "What is your approximate monthly income? (in term of thousands)",
-        "name":"income"
+      category: "Mental Health History",
+      choices: ["yes", "no"],
+      question: "Have you acted on your thoughts recently?",
+      name: "thoughts_acted",
     },
     {
-        "category": "Loan",
-        "choices": ["yes", "no"],
-        "question": "Do you have any outstanding loans?",
-        "name":"loan"
+      category: "Mental Health History",
+      choices: ["yes", "no"],
+      question:
+        "Have you ever acted on your thoughts of harming yourself or others?",
+      name: "thoughts_acted2",
     },
     {
-        "category": "Social and Emotional Well-being",
-        "choices": ["none", "1", "2", "3", "3+"],
-        "question": "How many close friends do you have?",
-        "name":"friend_no"
+      category: "Mental Health History",
+      choices: ["morning", "afternoon", "evening", "night"],
+      question:
+        "During which time of the day do you experience intrusive thoughts?",
+      name: "thoughts_time",
     },
     {
-        "category": "Social and Emotional Well-being",
-        "choices": ["yes", "no", "maybe"],
-        "question": "Does your friends help you when you need help?",
-        "name":"friend_help"
+      category: "",
+      choices: ["yes", "no"],
+      question: "Do you hear voices that others cannot?",
+      name: "voices",
     },
     {
-        "category": "Social and Emotional Well-being",
-        "choices": ["0", "1", "2", "3", "3+"],
-        "question": "How many times a week do you interact with your friends?",
-        "name":"friend_interact"
+      category: "Mental Health History",
+      choices: ["yes", "no"],
+      question: "Have you ever intentionally harmed others?",
+      name: "harming_others",
     },
     {
-        "category": "Social and Emotional Well-being",
-        "choices": ["yes", "no", "maybe", "idk"],
-        "question": "Do you feel comfortable sharing your feelings with others?",
-        "name":"share_feel"
+      category: "Mental Health History",
+      choices: ["yes", "no"],
+      question: "Do you have tried to commit suicide?",
+      name: "suicide",
     },
-    {
-        "category": "Social and Emotional Well-being",
-        "choices": ["yes", "no", "maybe"],
-        "question": "Do you have someone to talk to when you need support?",
-        "name":"have_someone"
-    },
-    {
-        "category": "Social and Emotional Well-being",
-        "choices": ["never", "maybe", "no", "sometimes", "yes"],
-        "question": "Do you often feel lonely?",
-        "name":"lonely"
-    },
-    {
-        "category": "Social and Emotional Well-being",
-        "choices": ["yes", "no"],
-        "question": "Have you ever been bullied?",
-        "name":"bullied"
-    },
-    {
-        "category": "Social and Emotional Well-being",
-        "choices": ["yes", "no", "maybe"],
-        "question": "Do you feel supported by your family?",
-        "name":"family_support"
-    },
-    {
-        "category": "Behavioral Patterns",
-        "choices": ["yes", "no"],
-        "question": "Do you compare your life to others?",
-        "name": "compare_life"
-    },
-    {
-        "category": "Behavioral Patterns",
-        "choices": ["<1 hour", "<2 hours", "<3 hours", "3+ hours"],
-        "question": "How much time do you spend on social media daily?",
-        "name":"social_media"
-
-    },
-    {
-        "category": "Sleep and Lifestyle",
-        "choices": ["none", "1", "2", "3", "3+"],
-        "question": "How many times a week do you hang out with friends?",
-        "name":"hangout"
-    },
-    {
-        "category": "Sleep and Lifestyle",
-        "choices": ["<10", "<15", "<20", "<24", "24"],
-        "question": "How many hours a day do you stay at home?",
-        "name":"home_time"
-    },
-    {
-        "category": "Sleep and Lifestyle",
-        "choices": ["yes", "no"],
-        "question": "Do you consider your self religious/spiritual?",
-        "name":"religious"
-    },
-    {
-        "category": "Goals and Outlook",
-        "choices": ["yes", "no", "maybe"],
-        "question": "Do you have specific goals for yourself?",
-        "name":"goal"
-    },
-    {
-        "category": "Mental Health History",
-        "choices": ["yes", "no"],
-        "question": "Have you ever feel suicidal ?",
-        "name":"suicidal"
-    },
-    {
-        "category": "Sleep and Lifestyle",
-        "choices": ["yes", "no"],
-        "question": "Do you have a sleeping disorder?",
-        "name":"sleep_disorder"
-    },
-    
-    {
-        "category": "Emotional Well-being",
-        "choices": ["yes", "no", "maybe"],
-        "question": "Are you currently in love with someone?",
-        "name":"love_someone"
-    },
-    {
-        "category": "Emotional Well-being",
-        "choices": ["yes", "no", "maybe"],
-        "question": "Have you recently lost someone close to you?",
-        "name":"die_someone"
-    },
-    {
-        "category": "Mental Health History",
-        "choices": ["yes", "no", "maybe"],
-        "question": "Do you have thoughts that tell you to do things?",
-        "name":"thoughts_command"
-    },
-    {
-        "category": "Mental Health History",
-        "choices": ["yes", "no", "maybe"],
-        "question": "Have you ever engaged in self-harm?",
-        "name":"self_harm"
-    },
-    {
-        "category": "Mental Health History",
-        "choices": ["yes", "no"],
-        "question": "Have you acted on your thoughts recently?",
-        "name":"thoughts_acted"
-    },
-    {
-        "category": "Mental Health History",
-        "choices": ["yes", "no"],
-        "question": "Have you ever acted on your thoughts of harming yourself or others?",
-        "name":"thoughts_acted2"
-    },
-    {
-        "category": "Mental Health History",
-        "choices": ["morning", "afternoon", "evening", "night"],
-        "question": "During which time of the day do you experience intrusive thoughts?",
-        "name":"thoughts_time"
-    },
-    {
-        "category": "",
-        "choices": ["yes", "no"],
-        "question": "Do you hear voices that others cannot?",
-        "name":"voices"
-    },
-    {
-        "category": "Mental Health History",
-        "choices": ["yes", "no"],
-        "question": "Have you ever intentionally harmed others?",
-        "name":"harming_others"
-    },
-    {
-        "category": "Mental Health History",
-        "choices": ["yes", "no"],
-        "question": "Do you have suicidal intent?",
-        "name":"suicide"
-    },
-    {
-        "category": "Mental Health History",
-        "choices": ["yes", "no"],
-        "question": "Do you often have suicidal thoughts?",
-        "name":"suicidal_thoughts"
-    },
-    {
-        "category": "Treatment",
-        "choices": ["yes", "no"],
-        "question": "Have you ever sought therapy or counseling?",
-        "name":"therapy"
-    }
-
-
+  {
+    category: "Mental Health History",
+    choices: ["yes", "no"],
+    question: "Do you often have suicidal thoughts?",
+    name: "suicidal_thoughts",
+  },
+  {
+    category: "Treatment",
+    choices: ["yes", "no"],
+    question: "Have you ever sought therapy or counseling?",
+    name: "therapy",
+  },
 ];
 
 const Assessment = () => {
@@ -229,7 +225,8 @@ const Assessment = () => {
   const [choice, setChoice] = useState("");
   const [score, setScore] = useState(0);
   const [isloading, setIsloading] = useState(true);
- 
+  const [resultData, setResultData] = useState({})
+
   const [formData, setFormData] = useState({
     gender: "",
     age: "",
@@ -264,7 +261,7 @@ const Assessment = () => {
     harming_others: "",
     suicide: "",
     suicidal_thoughts: "",
-    therapy: ""
+    therapy: "",
   });
 
   const fetchData = async () => {
@@ -277,28 +274,60 @@ const Assessment = () => {
     }
   };
 
+  const fetchAssessmentResult = async () => {
+    try {
+      const response = await axios.post(
+        "/predict",
+        formData, // Assuming formData contains the assessment data
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+  
+      console.log("Assessment result:", response.data);
+      return response.data; // Return the response data if needed
+    } catch (error) {
+      console.error("Error fetching assessment result:", error.message);
+      // Handle errors gracefully
+      return null;
+    }
+  };
+
   const handleSelectAnswer = (answer) => setChoice(answer);
 
-const handleClickNext = () => {
-  const currentQuiz = quizzes[quizNo];
-  const name = currentQuiz.name;
-  
-  // Check if a choice is selected
-  const selectedChoice = choice !== "" ? choice : "No answer";
+  const handleClickNext = () => {
+    const currentQuiz = quizzes[quizNo];
+    const name = currentQuiz.name;
 
-  setFormData({
-    ...formData,
-    [name]: selectedChoice
-  });
+    // Check if a choice is selected
+    const selectedChoice = choice !== "" ? choice : "No answer";
 
-  setChoice("")
-  
-  setQuizNo(quizNo + 1);
-};
+    setFormData({
+      ...formData,
+      [name]: selectedChoice,
+    });
 
-  
+    setChoice("");
+    setQuizNo(quizNo + 1);
 
-   
+    if (quizNo === quizzes.length - 1) {
+      const lastQuizName = currentQuiz.name;
+      const lastQuizAnswer = selectedChoice;
+      console.log("Last quiz reached:", {
+        ...formData,
+        [lastQuizName]: lastQuizAnswer,
+      });
+      setIsloading(true);
+      fetchAssessmentResult().then((data) => {
+        console.log(data);
+        setResultData(data)
+        setIsloading(false);
+      });
+    } else {
+    }
+  };
 
   const handleClickTry = () => {
     // setIsloading(true);
@@ -309,7 +338,7 @@ const handleClickNext = () => {
     //   setQuizzes(data);
     //   setIsloading(false);
     // });
-    console.log(formData)
+    console.log(formData);
   };
 
   useEffect(() => {
@@ -322,11 +351,7 @@ const handleClickNext = () => {
   const currentQuiz = quizzes.length > 0 && quizzes[quizNo];
 
   const choices = currentQuiz?.choices;
-  const answers = choices
-
-  
-
-  
+  const answers = choices;
 
   const quizTitleStyle =
     " w-full text-slate-800 dark:text-slate-100 fixed top-0 left-0 p-5 tracking-widest flex items-center justify-between";
@@ -340,8 +365,8 @@ const handleClickNext = () => {
           <Loader />
         ) : quizNo === quizzes.length ? (
           <ResultPage
-            score={score}
-            quizzes={quizzes}
+            prediction={resultData.prediction}
+            score={resultData.score}
             onClickTry={handleClickTry}
           />
         ) : (
